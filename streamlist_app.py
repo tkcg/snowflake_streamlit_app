a8b4896b455a17e3038990e3d03f39cc9d404a39
tkcg <92740807+tkcg@users.noreply.github.com>
2023-03-02 09:49:16 -0500

    Update streamlist_app.py
    changed location of dataframe

--- streamlist_app.py
+++ streamlist_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -13,3 +12,4 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
